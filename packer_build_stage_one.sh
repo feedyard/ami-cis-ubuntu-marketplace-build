b445219f8b4aa5f9da0cb8215eb85bb654f93468
ncheneweth <nchenewe@thoughtworks.com>
2018-05-08 14:25:52 -0500

    test

--- packer_build_stage_one.sh
+++ packer_build_stage_one.sh
@@ -8,7 +8,3 @@ sudo DEBIAN_FRONTEND=noninteractive pam-auth-update --package --force
 # noninteractive dist-upgrade will maintain existing secure config file settings
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y update
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::='--force-confdef' -o Dpkg::Options::='--force-confold' dist-upgrade
-
-# remove unnecessary packages
-sudo DEBIAN_FRONTEND=noninteractive apt-get -y auto-remove
-dpkg --list | grep linux-image | awk '{ print $2 }' | sort -V | sed -n '/'`uname -r`'/q;p' | xargs sudo apt-get -y purge
--- packer_build_stage_two.sh
+++ packer_build_stage_two.sh
@@ -2,6 +2,10 @@
 
 sudo lsof /var/lib/dpkg/lock
 
+# remove unnecessary packages
+sudo DEBIAN_FRONTEND=noninteractive apt-get -y auto-remove
+dpkg --list | grep linux-image | awk '{ print $2 }' | sort -V | sed -n '/'`uname -r`'/q;p' | xargs sudo apt-get -y purge
+
 # add new repository info
 cat <<EOF >>  sudo tee /etc/apt/sources.list
 deb http://us-east-1.ec2.archive.ubuntu.com/ubuntu/ artful main restricted
