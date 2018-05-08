1986b52695916a740d4e4284ae9ae0b452186710
ncheneweth <nchenewe@thoughtworks.com>
2018-05-08 18:41:49 -0500

    test

--- packer_build_stage_two.sh
+++ packer_build_stage_two.sh
@@ -8,25 +8,25 @@ dpkg --list | grep linux-image | awk '{ print $2 }' | sort -V | sed -n '/'`uname
 
 # add new repository info
 sudo bash -c 'cat <<EOF >> /etc/apt/sources.list
-deb http://us-east-1.ec2.archive.ubuntu.com/ubuntu/ artful main restricted
-deb http://security.ubuntu.com/ubuntu artful-security main restricted
+deb http://us-east-1.ec2.archive.ubuntu.com/ubuntu/ bionic main restricted
+deb http://security.ubuntu.com/ubuntu bionic-security main restricted
 EOF'
 
 sudo bash -c 'cat <<EOF > /etc/apt/preferences.d/cis_patch.pref
 Package: *
-Pin: release n=artful
+Pin: release n=bionic
 Pin-Priority: -10
 
 Package: man-db
-Pin: release n=artful
+Pin: release n=bionic
 Pin-Priority: 500
 
 Package: git
-Pin: release n=artful
+Pin: release n=bionic
 Pin-Priority: 500
 
 Package: cron
-Pin: release n=artful
+Pin: release n=bionic
 Pin-Priority: 500
 EOF'
 
