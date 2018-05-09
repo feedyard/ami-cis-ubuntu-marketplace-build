32a17c3e6dc997043834f85777aa87560f5e1226
ncheneweth <nchenewe@thoughtworks.com>
2018-05-09 09:45:23 -0500

    test

--- packer_build_stage_two.sh
+++ packer_build_stage_two.sh
@@ -8,25 +8,33 @@ dpkg --list | grep linux-image | awk '{ print $2 }' | sort -V | sed -n '/'`uname
 
 # add new repository info
 sudo bash -c 'cat <<EOF >> /etc/apt/sources.list
-deb http://us-east-1.ec2.archive.ubuntu.com/ubuntu/ bionic main restricted
-deb http://security.ubuntu.com/ubuntu bionic-security main restricted
+deb http://us-east-1.ec2.archive.ubuntu.com/ubuntu/ artful main restricted
+deb http://security.ubuntu.com/ubuntu artful-security main restricted
 EOF'
 
 sudo bash -c 'cat <<EOF > /etc/apt/preferences.d/cis_patch.pref
 Package: *
-Pin: release n=bionic
+Pin: release n=artful
 Pin-Priority: -10
 
+Package: libpipeline1
+Pin: release n=artful
+Pin-Priority: 500
+
 Package: man-db
-Pin: release n=bionic
+Pin: release n=artful
+Pin-Priority: 500
+
+Package: git-man
+Pin: release n=artful
 Pin-Priority: 500
 
 Package: git
-Pin: release n=bionic
+Pin: release n=artful
 Pin-Priority: 500
 
 Package: cron
-Pin: release n=bionic
+Pin: release n=artful
 Pin-Priority: 500
 EOF'
 
@@ -34,6 +42,8 @@ EOF'
 sudo apt-get -y update
 
 # specific package patch
+sudo apt-get install -y libpipeline1
 sudo apt-get install -y man-db
+sudo apt-get install -y git-man
 sudo apt-get install -y git
 sudo apt-get install -y cron
